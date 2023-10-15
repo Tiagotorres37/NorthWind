0d5a24f554071b4cebba2de752b455e53f6a24da
Tiagotorres37 <santiagotorresarenas2018@gmail.com>
2023-10-14 20:16:45 -0500

    Crear carpeta de Reposiories y su clases correspondientes

---
+++ NorthWind.Repositories.EFCore/Repositories/OrderRepository.cs
@@ -0,0 +1,36 @@
+﻿using NorthWind.Entities.Interfaces;
+using NorthWind.Entities.POCOEntities;
+using NorthWind.Entities.Specifications;
+using NorthWind.Repositories.EFCore.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Repositories.EFCore.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        readonly NorthWindContext Context;
+
+        public OrderRepository(NorthWindContext context) =>
+        Context = context;
+        
+        public void Create(Order order)
+        { 
+            Context.Add(order);
+        }
+
+        public void CreateOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Order> GetOrdersBySpecification(Specification<Order> specification)
+        {
+            var ExpressionDelegate = specification.Expression.Compile();
+            return Context.Orders.Where(ExpressionDelegate);
+        }
+    }
+}
