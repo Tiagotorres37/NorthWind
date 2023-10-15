6ace398f1dc90a9e93c67ec9f5913a91dc635538
Tiagotorres37 <santiagotorresarenas2018@gmail.com>
2023-10-14 20:27:26 -0500

    Proyecto Finalizado

---
+++ NorthWind.Controllers/OrderController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using NorthWind.Presenters;
+using NorthWind.UseCases.CreateOrder;
+using NorthWind.UseCasesDTOs.CreateOrder;
+using NorthWind.UseCasesPorts.CreateOrder;
+
+namespace NorthWind.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController
+    {
+        readonly ICreateOrderInputPort InputPort;
+        readonly ICreateOrderOutputPort OutputPort;
+
+        public OrderController(ICreateOrderInputPort inputPort,
+            ICreateOrderOutputPort outputPort) =>
+            (InputPort, OutputPort) = (inputPort, outputPort);
+
+        [HttpPost("create-order")]
+
+        public async Task<string> CreateOrder(
+            CreateOrderParams orderparams)
+        {
+            await InputPort.Handle(orderparams);
+            var Presenter = OutputPort as CreateOrderPresenter;
+            return Presenter.Content;
+        }
+    }
+}
