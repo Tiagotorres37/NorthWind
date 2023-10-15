ce308ae4c7f5a51667a0dd4c073a02fafd959561
Tiagotorres37 <santiagotorresarenas2018@gmail.com>
2023-10-14 20:24:59 -0500

    Completar NorthWind.Presenters

---
+++ NorthWind.Presenters/CreateOrderPresenter.cs
@@ -0,0 +1,20 @@
+﻿using NorthWind.UseCasesPorts.CreateOrder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Presenters
+{
+    public class CreateOrderPresenter : ICreateOrderOutputPort, IPresenter<string>
+    {
+        public string Content { get; private set; }
+
+        public Task Handle(int orderId) 
+        {
+            Content = $"Order ID: {orderId}";
+            return Task.CompletedTask;
+        }
+    }
+}
