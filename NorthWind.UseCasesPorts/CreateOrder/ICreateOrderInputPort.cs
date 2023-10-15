b70c7fd0969d92de33b99c33e86211a9512accb2
Tiagotorres37 <santiagotorresarenas2018@gmail.com>
2023-10-14 20:21:47 -0500

    Terminar carpete UseCasesPorts

---
+++ NorthWind.UseCasesPorts/CreateOrder/ICreateOrderInputPort.cs
@@ -0,0 +1,15 @@
+﻿using NorthWind.UseCasesDTOs.CreateOrder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.UseCasesPorts.CreateOrder
+{
+    public interface ICreateOrderInputPort
+    {
+        Task Handle(CreateOrderParams order);
+        Task Handle(int id);
+    }
+}
